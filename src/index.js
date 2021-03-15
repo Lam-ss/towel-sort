943834991b98a6be965f91a7fd1024c46be62770
Lam L <ppoo44ttaa@gmail.com>
2021-03-15 20:56:31 +0300

    task towel-sort completed

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if(!matrix || !matrix.length) return [];
+  let ss = matrix.map((currentValue, index) => {
+    if(index % 2 === 0) {
+      return currentValue.sort((x,y)=>x-y);
+    }
+    return currentValue.sort((x,y)=> y-x)
+  }).reduce((accumulator, currentValue) => accumulator.concat(currentValue), []);
+  return ss;
 }
